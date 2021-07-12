3b8d82b07334f7349d35f28b74ad9028f4d4d293
randall.erasmus <randallerasmus1@gmail.com>
2021-07-13 00:56:35 +0200

    Applying the router guard to the admin component only

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import {LoginComponent} from './components/login/login/login.component';
 import {AppComponent} from './app.component';
 import {AdminComponent} from './admin/admin.component';
 import {AuthComponent} from './auth/auth.component';
+import {AuthGuard} from './auth.guard';
 
 const routes: Routes = [
 
   { path: 'login', component: LoginComponent },
   { path: '', pathMatch: 'full', redirectTo: 'login'},
   { path: 'auth', component: AuthComponent },
-  { path: 'admin', component: AdminComponent }
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] }
 
 ];
 
