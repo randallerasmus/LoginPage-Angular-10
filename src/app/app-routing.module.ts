397fe99729c9faf1f7f397f137d7ed2ffa2d7ccd
randall.erasmus <randallerasmus1@gmail.com>
2021-07-13 00:43:49 +0200

    Update the routing module ts for the two components that was added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './components/login/login/login.component';
 import {AppComponent} from './app.component';
+import {AdminComponent} from './admin/admin.component';
+import {AuthComponent} from './auth/auth.component';
 
 const routes: Routes = [
 
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'login'},
+  { path: 'auth', component: AuthComponent },
+  { path: 'admin', component: AdminComponent }
 
 ];
 
